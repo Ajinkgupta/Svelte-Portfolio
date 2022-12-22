1658201d002885dd275c6c5d513b60aed806d8d4
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-12-22 12:59:48 +0530

    Update projects.svelte

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -44,8 +44,10 @@
 			   <br>
 			   <div class="inline-flex rounded-md shadow-sm" role="group"><a href="{`/project/${project.slug}`}"><button type="button" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">Read More 
     </button></a>
-    <a href="{project.gitub}"><button type="button" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">Github
-    </button></a></div>
+    <a href="{project.gitub}" >
+	    <button type="button" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">Github
+    </button>
+				   </a></div>
                      
                    </div>
                  </div>
