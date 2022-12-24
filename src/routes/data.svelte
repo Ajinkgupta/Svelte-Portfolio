49ee658446f75ea14276cd57a0687768ac4326c2
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-12-24 15:52:29 +0530

    Update data.svelte

--- src/routes/data.svelte
+++ src/routes/data.svelte
@@ -1,16 +1,13 @@
 <script>
   import { onMount } from "svelte";
-  const endpoint = "https://jsonplaceholder.typicode.com/posts";
+  const endpoint = "https://ajinkgupta.me/ajinkgupta/projects/data.json";
   let posts = [];
-
   onMount(async function () {
     const response = await fetch(endpoint);
     const data = await response.json();
     console.log(data);
   });
- 
   posts = data;
-
 </script>
 {#each posts as article}
   <div>
