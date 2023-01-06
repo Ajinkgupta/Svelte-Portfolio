33ca9b88f1f2a9d79bca7e2d6a4255e578f8062d
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-01-07 00:53:23 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,7 @@
 import adapter from '@sveltejs/adapter-netlify'
 import path from 'path'
-<<<<<<< HEAD
 import preprocess from 'svelte-preprocess'
  
-=======
-import preprocess from 'svelte-preprocess' 
-
->>>>>>> 8ea29bee8c3968b53aff08dce16f803f2b7be1f9
 const config = {
   kit: {
     adapter: adapter(),
@@ -26,7 +21,4 @@ const config = {
 }
 
 export default config
-<<<<<<< HEAD
  
-=======
->>>>>>> 8ea29bee8c3968b53aff08dce16f803f2b7be1f9
