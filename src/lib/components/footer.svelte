4125341efa51d8c2d99cea6b3881f488da8e1261
Ajinkgupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-01-07 00:59:14 +0530

    commit

--- src/lib/components/footer.svelte
+++
@@ -1,8 +0,0 @@
-<div class="flex  fixed  bottom-0 left-1/2 -translate-y-1/2 -translate-x-1/2
-items-center justify-center">
-  <div class="inline-flex shadow-md hover:shadow-lg focus:shadow-lg" role="group">
-    <a href="/">  <button type="button" class="rounded-l inline-block px-6 py-2.5 bg-[#f6ff00] text-black font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">🏠</button> </a>
-   <a href="/projects">  <button type="button" class=" inline-block px-6 py-2.5 bg-[#f6ff00] text-black font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Projects</button></a>
-    <a href="/posts">   <button type="button" class=" rounded-r inline-block px-6 py-2.5 bg-[#f6ff00] text-black font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Blog</button></a>
-  </div>
-</div>
--- src/lib/graphql-client.js
+++
@@ -1,3 +0,0 @@
-import { GraphQLClient } from 'graphql-request'
-const HYGRAPH_ENDPOINT = 'https://ap-south-1.cdn.hygraph.com/content/clck9n9w011h201rr1chgd0af/master'
-export const client = new GraphQLClient(HYGRAPH_ENDPOINT)
