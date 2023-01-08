ff281bcdb8633773dbc557ee95ff8611d02706b5
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-01-08 16:45:44 +0530

    Update footer.svelte

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,6 +3,6 @@ items-center justify-center">
   <div class="inline-flex shadow-lg shadow-[black]	 hover:shadow-lg focus:shadow-lg" role="group">
     <a href="/">  <button type="button" class="rounded-l inline-block px-6 py-2.5 bg-[#f6ff00] text-black font-bold text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">🏠</button> </a>
    <a href="/projects">  <button type="button" class=" inline-block px-6 py-2.5 bg-[#f6ff00] text-black font-bold text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Projects</button></a>
-    <a href="/posts">   <button type="button" class=" rounded-r inline-block px-6 py-2.5 bg-[#f6ff00] text-black font-bold text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Blog</button></a>
+    <a href="/blog">   <button type="button" class=" rounded-r inline-block px-6 py-2.5 bg-[#f6ff00] text-black font-bold text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Blog</button></a>
   </div>
 </div>
