c0437dd685fc311c9d2f9b2553d245a5e8761a20
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-01-08 16:43:30 +0530

    Update and rename src/routes/posts/+page.svelte to src/routes/blog/+page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -66,7 +66,7 @@
       />
       <div class="flex">
         
-        <a href={`/posts/${slug}`}   >   <button type="button" class="rounded-3xl font-bold inline-block px-6 py-2.5 bg-[#f6ff00] opacity-80	 text-black  text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Read </button></a>
+        <a href={`/blog/${slug}`}   >   <button type="button" class="rounded-3xl font-bold inline-block px-6 py-2.5 bg-[#f6ff00] opacity-80	 text-black  text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Read </button></a>
       
       </div>
        
