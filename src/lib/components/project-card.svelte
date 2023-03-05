bb471dd47e28b8d12ee633a306b81993408bd14e
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-03-05 19:47:58 +0530

    Update project-card.svelte

--- src/lib/components/project-card.svelte
+++ src/lib/components/project-card.svelte
@@ -29,7 +29,7 @@
       />
       <div class="flex">
         
-        <a href={`/projects/${slug}`}>  <button type="button" class="rounded-3xl font-bold inline-block px-6 py-2.5 bg-[#f6ff00] opacity-80	 text-black  text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Explore</button></a>
+        <a href={`/projects/${slug}`}>  <button type="button" class="rounded-3xl font-bold inline-block px-6 py-2.5 bg-[#a787ff] opacity-80	 text-black  text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Explore</button></a>
       
       </div>
        
