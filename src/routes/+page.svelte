3185feba90d72a5fb16bd5a4d9eaa09e6c803fba
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-03-05 19:46:40 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,7 +35,7 @@
       <h1 class="text-3xl mb-4 font-bold tracking-wider">{name}</h1>
       <p class="text-xl mb-4">{intro}</p>
        
-      <a href="/about">  <button type="button" class="rounded-3xl font-bold inline-block px-6 py-2.5 bg-[#f6ff00] text-black  text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">About me</button></a>
+      <a href="/about">  <button type="button" class="rounded-3xl font-bold inline-block px-6 py-2.5 bg-[#a787ff] text-black  text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">About me</button></a>
    
      </div>
   </div>
